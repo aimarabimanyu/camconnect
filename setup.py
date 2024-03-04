c44ee662b9ac57203807045b526cce156cf13efd
Aimar Abimayu Pratama <aimarabimanyu123@gmail.com>
2024-03-05 06:21:57 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 DESCRIPTION = 'Simple Interface for Camera in Python'
 LONG_DESCRIPTION = 'A simple interface for connect, display, and interaction with the camera in Python using OpenCV and Tkinter.'
 
